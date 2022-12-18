63cb411e4e90660b9901735de518498cbd7e04f9
Lizter <lizterdsouza@gmail.com>
2022-12-18 23:24:22 +0530

    codefinal

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -33,31 +33,31 @@ public class F1 extends Car {
         if(newSpeed > 250) {
 
             changeGear(6);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
         else if(newSpeed > 200) {
 
             changeGear(5);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
         else if(newSpeed > 150) {
 
             changeGear(4);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
         else if(newSpeed > 100) {
 
             changeGear(3);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
         else if(newSpeed > 50) {
 
             changeGear(2);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
         else if(newSpeed > 0) {
 
-            changeGear(1);
+
             changeSpeed(newSpeed, getCurrentDirection());
 
         }
